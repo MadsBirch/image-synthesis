0af92cd8fe534c4980c977f472be60ed78b0bc5e
Mads Birch Sørensen <mbs@research-1@cerebriu.com>
2023-09-01 09:26:06 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project investigates to what degree deep learning models can be used to synthesize post-contrast MRI from pre-constrast MRI.',
+    author='Mads',
+    license='',
+)
